774e67e0380c8743d42210087161f4d526b04297
emaresmoreno <emaresmoreno@ucsd.edu>
2022-05-01 21:05:39 -0700

    Fixed error in code

--- SkillDemoMid.java
+++ SkillDemoMid.java
@@ -1,5 +1,5 @@
 public class SkillDemoMid{
     public static int divide(int a, int b){
-        return a % b;
+        return a / b;
     }
 }
--- SkillDemoMidTester.java
+++ SkillDemoMidTester.java
@@ -4,7 +4,8 @@ import org.junit.*;
 public class SkillDemoMidTester {
     @Test
     public void test(){
-        assertEquals("Checks 4 is correctly divided by 2", 2, SkillDemoMid.divide(4,2));
+        assertEquals("Checks 4 is correctly divided by 2", 
+        2, SkillDemoMid.divide(4,2));
 
     }
 }
