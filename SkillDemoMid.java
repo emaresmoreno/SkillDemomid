83a1d58aff6c385c67d841b704c1397a023fbecb
emaresmoreno <emaresmoreno@ucsd.edu>
2022-05-01 21:02:00 -0700

    Created a tester with error result

--- SkillDemoMid.java
+++ SkillDemoMid.java
@@ -1,2 +1,5 @@
 public class SkillDemoMid{
+    public static int divide(int a, int b){
+        return a % b;
+    }
 }
---
+++ SkillDemoMidTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoMidTester {
+    @Test
+    public void test(){
+        assertEquals("Checks 4 is correctly divided by 2", 2, SkillDemoMid.divide(4,2));
+
+    }
+}
